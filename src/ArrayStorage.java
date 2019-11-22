59634809f261a6de0766f7666265ad52a59497e5
Juri Smirnov <juri.smirnov@gmail.com>
2019-11-22 23:51:07 +0100

    First try

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,59 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int index=0; //shows the position of first null
 
+    //clear all values in storage (change to null)
     void clear() {
+        for(int i=0;i<index;i++){ storage[i]=null;
+        }
+        index=0;
     }
 
+    //put resume in the storage
     void save(Resume r) {
+        //here could we check that index do not exceed the array length "if index<9999" or "try catch"
+        storage[index]= r;
+        index++;
     }
 
+    //returns the resume by uuid
     Resume get(String uuid) {
+        for(int i=0;i<index;i++){
+            if(storage[i].uuid.equals(uuid)){
+                return storage[i];
+            }
+        }
         return null;
     }
 
+    //deletes the resume by uuid
     void delete(String uuid) {
+        for(int i=0;i<index;i++){
+            if(storage[i].uuid.equals(uuid)){
+                storage[i]=storage[index-1];
+                storage[index-1]=null;
+                index--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        //Resume[] tmpStorage = new Resume[index];
+        //for(int i=0;i<index;i++){
+        //   tmpStorage[i]=storage[i];
+        //}
+        return Arrays.copyOfRange(storage,0, index);
     }
 
     int size() {
-        return 0;
+        return index;
     }
 }
