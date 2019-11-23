4d41bdf047e5fd483dde96eabb811b39740689c4
Juri Smirnov <juri.smirnov@gmail.com>
2019-11-23 09:46:34 +0100

    First try

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,25 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
-    private int index=0; //shows the position of first null
+    private int index = 0; //shows the position of first null
 
     //clear all values in storage (change to null)
     void clear() {
-        for(int i=0;i<index;i++){ storage[i]=null;
-        }
-        index=0;
+        Arrays.fill(storage, 0, index, null);
+        index = 0;
     }
 
     //put resume in the storage
     void save(Resume r) {
         //here could we check that index do not exceed the array length "if index<9999" or "try catch"
-        storage[index]= r;
+        storage[index] = r;
         index++;
     }
 
     //returns the resume by uuid
     Resume get(String uuid) {
-        for(int i=0;i<index;i++){
-            if(storage[i].uuid.equals(uuid)){
+        for (int i = 0; i < index; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
@@ -33,10 +32,10 @@ public class ArrayStorage {
 
     //deletes the resume by uuid
     void delete(String uuid) {
-        for(int i=0;i<index;i++){
-            if(storage[i].uuid.equals(uuid)){
-                storage[i]=storage[index-1];
-                storage[index-1]=null;
+        for (int i = 0; i < index; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[index - 1];
+                storage[index - 1] = null;
                 index--;
             }
         }
@@ -46,11 +45,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        //Resume[] tmpStorage = new Resume[index];
-        //for(int i=0;i<index;i++){
-        //   tmpStorage[i]=storage[i];
-        //}
-        return Arrays.copyOfRange(storage,0, index);
+        return Arrays.copyOf(storage, index);
     }
 
     int size() {
