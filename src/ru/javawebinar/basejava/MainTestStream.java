e5925198ebdab228a751b93c28702e533c0e7938
Juri Smirnov <juri.smirnov@gmail.com>
2020-01-14 23:07:29 +0100

    HW12 v.1
    MainTestStream.class:
     minValue2() method added

--- src/ru/javawebinar/basejava/MainTestStream.java
+++ src/ru/javawebinar/basejava/MainTestStream.java
@@ -17,6 +17,12 @@ public class MainTestStream {
         return (int) IntStream.range(0, input.size()).mapToDouble(i -> input.get(i) * Math.pow(10, i)).sum();
     }
 
+    private static int minValue2(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted().reduce((x, y) -> x * 10 + y).getAsInt();
+    }
+
     private static List<Integer> oddOrEven(List<Integer> integers) {
         boolean even = integers.stream().reduce(Integer::sum).orElse(0) % 2 != 0;
         return integers.stream().filter(even ? x -> x % 2 != 0 : x -> x % 2 == 0).collect(Collectors.toList());
@@ -25,6 +31,10 @@ public class MainTestStream {
     public static void main(String[] args) {
         System.out.println(minValue(new int[]{1, 4, 1, 2, 2, 4}));
         System.out.println(minValue(new int[]{1, 4, 1, 3, 2, 4, 8}));
+
+        System.out.println(minValue2(new int[]{1, 4, 1, 2, 2, 4}));
+        System.out.println(minValue2(new int[]{1, 4, 1, 3, 2, 4, 8}));
+
         System.out.println(oddOrEven(Arrays.asList(1, 4, 1, 2, 2, 4)));
         System.out.println(oddOrEven(Arrays.asList(1, 4, 3, 1, 2, 2, 4)));
     }
